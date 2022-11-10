18e3e519006f418d8a0816ff41bbb18ce57b9fea
pschutte1107 <116783341+pschutte1107@users.noreply.github.com>
2022-11-10 14:33:01 -0600

    Add files via upload

--- main.py
+++ main.py
@@ -3,3 +3,19 @@ things[1] = things[1].capitalize()
 things[0] = things[0].upper()
 del things[2]
 print(things)
+
+
+def good():
+    return ["Harry", "Hermione", "Ron"]
+
+
+print(good())
+
+
+get_odds = (num for num in range(1, 10) if not num % 2 == 0)
+count = 0
+for num in get_odds:
+    if count == 2:
+        print(num)
+        break
+    count += 1
